bfc4a12a89361c430969890eefa35fcf7163327c
phlintch <magnus96h@gmail.com>
2023-02-21 15:07:13 +0100

    Consump. by day statistics added on click

--- Stacc-Opg/Frontend/src/app/DayGraph/DayGraph.component.ts
+++ Stacc-Opg/Frontend/src/app/DayGraph/DayGraph.component.ts
@@ -10,6 +10,7 @@ import { BaseChartDirective } from 'ng2-charts';
   styleUrls: ['./DayGraph.component.scss'],
 })
 export class DayGraphComponent {
+
   @ViewChild(BaseChartDirective) chart!: BaseChartDirective;
 
   title = 'Consumption chart';
@@ -19,22 +20,25 @@ export class DayGraphComponent {
     datasets: [
       {
         data: [],
-        label: 'Day Average',
+        label: 'Average By Date',
         fill: true,
         tension: 0.5,
-        borderColor: 'green',
+        borderColor: 'rgb(50,200,50)',
         backgroundColor: 'rgba(50,200,50,0.3)',
       },
     ],
   };
   public lineChartOptions: ChartOptions<'line'> = {
     responsive: false,
+    maintainAspectRatio: true,
   };
   public lineChartLegend = true;
 
   public consumptions: Consumption[] = [];
   line: any;
-
+  lowestConsumptions: { date: Date; consumption: number; }[] = [];
+  highestConsumptions: { date: Date; consumption: number; }[] = [];
+  
   constructor(private consumptionService: ConsumptionService) {}
 
   ngOnInit() {
@@ -53,13 +57,34 @@ export class DayGraphComponent {
         day[0].from.toLocaleDateString()
       );
       this.lineChartData.datasets[0].data = this.toDays.map(
-        (day) => 
+        (day) =>
           day.reduce((sum, consumption) => sum + consumption.consumption, 0) /
-          day.length
+          24
       );
       if (this.chart && this.chart.chart) {
         this.chart.chart.update();
       }
+      this.highestConsumptions = this.toDays
+        .map((day) => ({
+          date: day[0].from,
+          consumption: day.reduce(
+            (sum, consumption) => sum + consumption.consumption,
+            0
+          ),
+        }))
+        .sort((a, b) => b.consumption - a.consumption)
+        .slice(0, 3);
+
+      this.lowestConsumptions = this.toDays
+        .map((day) => ({
+          date: day[0].from,
+          consumption: day.reduce(
+            (sum, consumption) => sum + consumption.consumption,
+            0
+          ),
+        }))
+        .sort((a, b) => a.consumption - b.consumption)
+        .slice(0, 3);
     });
   }
 
@@ -82,4 +107,8 @@ export class DayGraphComponent {
     });
     this.toDays.push(day);
   }
+  clicked: boolean = false;
+  chartClicked($event: { event?: import("chart.js").ChartEvent|undefined; active?: {}[]|undefined; }) {
+      this.clicked = !this.clicked;
+    }
 }
