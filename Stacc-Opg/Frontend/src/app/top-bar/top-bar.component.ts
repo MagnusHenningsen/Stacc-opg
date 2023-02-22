2c9bded94478d7b00a894c5f3d66df783e76e9e2
phlintch <magnus96h@gmail.com>
2023-02-22 13:49:56 +0100

    Slight polish

--- Stacc-Opg/Frontend/src/app/top-bar/top-bar.component.ts
+++ Stacc-Opg/Frontend/src/app/top-bar/top-bar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class TopBarComponent {
 
+  refresh() {
+    window.location.reload(); // Temporary since there is no home site currently, it's just assumed that in a real application there would be one.
+  }
 }
